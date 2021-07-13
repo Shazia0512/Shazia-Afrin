904e770762f9af3f81e2e9a1c6ba4bb86669b0e9
Shazia0512 <87201773+Shazia0512@users.noreply.github.com>
2021-07-13 23:23:15 +0530

    Add files via upload

---
+++ Switch_Case.java
@@ -0,0 +1,30 @@
+
+public class Switch_Case {
+	public static void main(String[] args) {
+		int ch=12;
+		switch(ch)
+		{
+		case 11: System.out.println("character");
+		break;
+		case 12: System.out.println("value");
+		}
+		
+     System.out.println("*********************************************************");
+     int tech =1;
+     int value=2;
+     switch(tech)
+     {
+     case 1: System.out.println("value of Integer is 1");
+     case 2:
+    	 switch(value)
+    	 {
+    	 case 1:System.out.println("Java");
+    	 break;
+    	 
+    	 case 2:System.out.println("Problem Solving");
+    	 }
+     }
+}
+	
+	
+}
